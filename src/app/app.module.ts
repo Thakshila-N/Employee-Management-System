2cafbf6089e3f80f2c1f431ae440ab9d053d0cd5
unknown <thakshila@konekt.lk>
2023-07-17 12:56:55 +0530

    Add Form Control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  // Implement the formGroup
+  loginForm! : FormGroup
+
+  constructor(
+    // inject form builder
+    private fb : FormBuilder
+  ){}
+
+  ngOnInit(): void {
+
+    this.loginForm = this.fb.group({
+      username : ['', Validators.required],
+      password : ['', Validators.required]
+    });
+
+  }
+
+  // This section related to the show and hide password section
   type:string ="password";
   isText: boolean=false;
   eyeIcon: string = "fa-eye-slash"
 
   hideShowPass(){
     this.isText = !this.isText;
-    // 
+    //
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+// end of the show and hide password section
+
+// onSubmit method
+onSubmit(){
+  if(this.loginForm.valid){
+
+    console.log(this.loginForm.value)
+
+  }else{
+
+  }
+}
 
 }
